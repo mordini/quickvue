5cf55a82af4dd1655546c8e02d672fda51c14a7c
mordini <yahewisthedevil@yahoo.com>
2022-07-22 14:55:42 +0000

    added standard default settings

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <TestComponent feature="X" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestComponent from './components/TestComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TestComponent,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    <h1>Use me for testing {{ feature }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  props: {
+    feature: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
